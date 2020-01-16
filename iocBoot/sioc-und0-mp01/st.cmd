8e38cb6ef58086d3249ad7b6eb653e90ad374d73
Jeremy Mock <jmock@lcls-dev3.slac.stanford.edu>
2020-01-16 09:36:37 -0800

    upgrate build to EPICS R7.0.2-1.1; update nodenames for production systems; update PV numbers for UNDH CBLMs

--- iocBoot/sioc-und0-mp01/st.cmd
+++ iocBoot/sioc-und0-mp01/st.cmd
@@ -42,10 +42,10 @@ CAENHV_registerRecordDeviceDriver(pdbbase)
 #     password)     # Password
 
 # Crate located in B913 (node name: hvps-und0-mp01)
-CAENHVAsynConfig("${CAENHVASYN_PORT_B913}",3,"172.27.7.104","admin","admin")
+CAENHVAsynConfig("${CAENHVASYN_PORT_B913}",3,"hvps-und0-mp01","admin","admin")
 
 # Crate located in B921 (node name: hvps-und0-mp02)
-CAENHVAsynConfig("${CAENHVASYN_PORT_B921}",3,"172.27.7.105","admin","admin")
+CAENHVAsynConfig("${CAENHVASYN_PORT_B921}",3,"hvps-und0-mp02","admin","admin")
 
 # ===========================================
 #               ASYN MASKS
